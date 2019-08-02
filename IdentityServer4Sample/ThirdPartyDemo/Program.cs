542ccb3c25f77adbe4cbf243aa08a18624ef0f94
余学亮 <szyuxueliang@hotmail.com>
2019-08-02 18:07:49 +0800

    新增第三方调用clientCredentialApi temp

---
+++ IdentityServer4Sample/ThirdPartyDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using IdentityModel.Client;
+
+namespace ThirdPartyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var diso = DiscoveryClient.GetAsync("http://localhost:5000").Result;
+            if (diso.IsError)
+            {
+                Console.WriteLine(diso.Error);
+            }
+            //var tokenClient 
+        }
+    }
+}
