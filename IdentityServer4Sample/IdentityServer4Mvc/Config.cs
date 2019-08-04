482271c5d6de9b68adf8a007548ba53c7217ce10
余学亮 <szyuxueliang@hotmail.com>
2019-08-04 11:32:56 +0800

    oauth2 + oidc 实现 server部分

---
+++ IdentityServer4Sample/IdentityServer4Mvc/Config.cs
@@ -0,0 +1,63 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Test;
+using System.Security.Claims;
+using IdentityModel;
+
+namespace IdentityServer4Mvc
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1","Api Application")
+            };
+            
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email()
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                     ClientId="mvc",
+                     AllowedGrantTypes=GrantTypes.Implicit,
+                     ClientSecrets = { new Secret("secret".Sha256()) },
+                     AllowedScopes={ "api1" }//和ApiResource 中的保持一致
+                },
+               
+            };
+
+        }
+
+        public static List<TestUser> GetTestUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                     SubjectId="1",
+                     Username = "yxl",
+                     Password ="123456",
+                    
+                }
+            };
+        }
+    }
+}
---
+++ IdentityServer4Sample/IdentityServer4Mvc/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using IdentityServer4;
+using Microsoft.AspNetCore.Authentication;
+using IdentityServer4Mvc.Models;
+using IdentityServer4.Test;
+
+namespace IdentityServer4Mvc.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly TestUserStore _users;
+
+        public AccountController(TestUserStore users)
+        {
+            _users = users;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login(string returnUrl)
+        {
+            ViewData["returnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model,string returnUrl)
+        {
+            var user = _users.FindByUsername(model.UserName);
+            if (user==null)
+            {
+                throw new Exception("用户名不存在");
+            }
+            else
+            {
+                if (_users.ValidateCredentials(model.UserName, model.Password))
+                {
+
+                    var props = new AuthenticationProperties
+                    {
+                        IsPersistent = true,
+                        ExpiresUtc = DateTimeOffset.UtcNow.Add(TimeSpan.FromMinutes(30))
+                    };
+                    await Microsoft.AspNetCore.Http.AuthenticationManagerExtensions.SignInAsync(HttpContext, user.SubjectId, user.Username, props);
+                    return Redirect(returnUrl??"/");
+                }
+                else
+                {
+                    throw new Exception("密码错误");
+                }
+            }
+         
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+
+            return Redirect("/");
+        }
+    }
+}
