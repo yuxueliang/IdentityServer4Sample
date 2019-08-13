8f6597c7b80d5c41d5fd26b90506c380d43a9fa4
余学亮 <szyuxueliang@hotmail.com>
2019-08-13 16:12:37 +0800

    晚上dockerfile

--- IdentityServer4Sample/WebNotebook/Controllers/HomeController.cs
+++ IdentityServer4Sample/WebNotebook/Controllers/HomeController.cs
@@ -4,14 +4,21 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NLog;
 using WebNotebook.Models;
 
 namespace WebNotebook.Controllers
 {
     public class HomeController : Controller
     {
+        public static Logger logger = LogManager.GetLogger("SimpleDemo");
+
         public IActionResult Index()
         {
+            var log = $"客户端：{HttpContext.Connection.RemoteIpAddress} 访问了 Index 页面！";
+
+            logger.Info(log);
             return View();
         }
 
